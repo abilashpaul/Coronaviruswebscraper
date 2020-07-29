d46070856a76bd66fd786c487339b0e4eed47313
abilashpaul <68249190+abilashpaul@users.noreply.github.com>
2020-07-28 23:52:47 -0500

    Add files via upload

---
+++ Coronavirus.py
@@ -0,0 +1,58 @@
+# importing modules 
+import requests 
+from bs4 import BeautifulSoup 
+import texttable as tt
+import pandas as pd
+  
+# URL for scrapping data 
+url = 'https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/'
+  
+# get URL html 
+page = requests.get(url) 
+soup = BeautifulSoup(page.text, 'html.parser') 
+  
+data = [] 
+  
+# soup.find_all('td') will scrape every element in the url's table 
+data_iterator = iter(soup.find_all('td'))  
+# data_iterator is the iterator of the table 
+  
+# This loop will keep repeating till there is data available in the iterator 
+while True: 
+    try: 
+        country = next(data_iterator).text 
+        confirmed = next(data_iterator).text 
+        deaths = next(data_iterator).text 
+        continent = next(data_iterator).text 
+  
+        # For 'confirmed' and 'deaths', make sure to remove the commas and convert to int 
+        data.append(( 
+            country, 
+            int( confirmed.replace(',', '') ), 
+            int( deaths.replace(',', '') ), 
+            continent 
+        )) 
+        
+    # StopIteration error is raised when there are no more elements left to iterate through 
+    except StopIteration: 
+        break
+  
+# Sort the data by the number of confirmed cases 
+data.sort(key = lambda row: row[1], reverse = True) 
+#To print the data in human readable format, we will use the library 'texttable'
+
+#filter_none
+#brightness_4
+# create texttable object 
+table = tt.Texttable() 
+table.add_rows([(None, None, None, None)] + data)  # Add an empty row at the beginning for the headers 
+table.set_cols_align(('c', 'c', 'c', 'c'))  # 'l' denotes left, 'c' denotes center, and 'r' denotes right 
+table.header((' Country ', ' Number of cases ', ' Deaths ', ' Continent ')) 
+  
+print(table.draw()) 
+
+
+
+
+df = pd.DataFrame([{"Country ":country ,"Number of cases ":confirmed ,"Deaths ":deaths, "Continent":continent }])
+df.to_csv('corona.csv', index=False, encoding='utf-8')
